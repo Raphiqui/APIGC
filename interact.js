b4e3f1aca993f218787c20ba65f528fa0528bde9
Norsse <raphael.pastre@gmail.com>
2019-10-26 13:53:10 +0200

    interact with few sample of records and API to insert dom color into db

--- interact.js
+++ interact.js
@@ -1,45 +1,50 @@
-// const mongoose = require('mongoose');
-// const dbURL = 'mongodb://127.0.0.1:27017/APIGCDB';
-//
-// const getURL = () => {
-//     return new Promise((resolve, reject) => {
-//         mongoose.connect(dbURL, (err, db) => {
-//             if (err) {
-//                 throw err;
-//             }
-//             const dbo = db.db;
-//
-//             dbo.collection('products').find({}).toArray((err, result) => {
-//                 if (err === null) {
-//                     console.log(result);
-//                     resolve(result[0]);
-//                 } else {
-//                     console.log(err);
-//                     reject(err);
-//                 }
-//             });
-//         });
-//     });
-// };
-//
-// const asyncFunction = async () => {
-//     const a = await getURL();
-//     console.log('A: ', a)
-// };
-//
-// asyncFunction();
+const mongoose = require('mongoose');
 const vision = require('@google-cloud/vision');
 const credentials = require('./data/credentials.json');
-
+const dbURL = 'mongodb://127.0.0.1:27017/APIGCDB';
 const client = new vision.ImageAnnotatorClient({
     credentials
 });
 
-async function quickstart() {
+async function fetchColor(image) {
     // Performs label detection on the image file
-    const [result] = await client.imageProperties('./data/test.jpg');
-    const colors = result.imagePropertiesAnnotation.dominantColors.colors;
-    colors.forEach(color => console.log(color));
+    const [result] = await client.imageProperties(image);
+    if (result !== null) {
+        const colors = result.imagePropertiesAnnotation.dominantColors.colors;
+        // colors.forEach(color => console.log(color));
+        return colors[0]
+    }
 }
 
-quickstart();
+const getURL = () => {
+    return new Promise((resolve, reject) => {
+        mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
+            if (err) {
+                throw err;
+            }
+            const dbo = db.db;
+
+            db.collection('products').find({}).limit(10).forEach(async doc => {
+
+                const photoUrl = 'http:' + doc.photo;
+                const promise = await fetchColor(photoUrl);
+                console.log("r:", promise);
+                const update = {dom_color: promise};
+                db.collection('products').updateOne(
+                    {_id: doc._id},
+                    {$set: update},
+                    {upsert: true, new: true}, (err, result) => {
+                        if (err) {
+                            console.log(err)
+                        }
+                    })
+            })
+        });
+    });
+};
+
+const asyncFunction = async () => {
+    const a = await getURL();
+};
+
+asyncFunction();
