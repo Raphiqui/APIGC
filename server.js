274513d8675eed53ea9a931294a4333a09efae88
Norsse <raphael.pastre@gmail.com>
2019-10-26 16:10:09 +0200

    install nodemon | create server for endpoint

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.get('/ping', (req, res) => {
+    res.send('pong')
+});
+
+app.listen(process.env.PORT || 3053, () => {
+    console.log("Listening on port: 3053")
+});
